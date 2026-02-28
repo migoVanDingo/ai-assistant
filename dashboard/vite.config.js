7e3560f181582dc7b474913808360f170ca9e4de
migo <migo.vandingo@gmail.com>
2026-02-28 10:29:03 -0500

    config

--- dashboard/vite.config.js
+++ dashboard/vite.config.js
@@ -1,10 +1,16 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+const allowedHosts = [
+  'node1.tailb058fe.ts.net',
+]
+
 export default defineConfig({
   plugins: [react()],
   server: {
+    host: '0.0.0.0',
     port: 5173,
+    allowedHosts,
     proxy: {
       '/api': {
         target: 'http://localhost:8000',
